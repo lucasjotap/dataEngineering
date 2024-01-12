3475855cf840832b76434f4f03fa62432c322fa1
lucasjotap <leiathemosthot@gmail.com>
2024-01-12 15:34:53 -0300

    second class finished

---
+++ segunda_aula/athena/athena.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+
+from pyathena import connect
+from typing import NoReturn
+from pandas import DataFrame 
+
+class DataReaderFromAthena(object):
+	"""DataReaderFromAthena lidará com queries vindo do Athena."""
+	conn: object = None
+	pd.set_option("display.width", 420)
+	pd.set_option("display.max_columns", 15)
+
+	def establish_connection(self) -> object:
+		return self.conn: object = connect(work_group='athena-data-engineer-workgroup')
+
+	def read_sql(self):
+		self.establish_connection()
+		query: str = "select * from data_lake_raw.titanic"
+		df: DataFrame = pd.read_sql(query, conn)
+		print(df.head())
